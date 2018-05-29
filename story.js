00888b41310d0431536ef022dc469d8ba3172bf4
bloomengine <37038355+bloomengine7@users.noreply.github.com>
2018-05-29 22:41:45 +0800

    z

--- story.js
+++ story.js
@@ -427,9 +427,9 @@ case "start": //aka caf
 
         //as more and more added, remove older state variables and only keep the newest ones. If ppl play for a while they will have already seen the old stuff
         if (f.thread_intro > 1 && f.thread_chess_club > 0) {
-
-            ga('send', 'pageview', document.location + "?" + "finished-memories");
-        
+            if (typeof ga !== "undefined") { 
+                ga('send', 'pageview', document.location + "?" + "finished-memories");
+            } 
             d+="\n\n<em>You have exhausted your memories.</em> \n<span style='font-size:.75em'>Please check back later for more content. <a href=\"https://feedburner.google.com/fb/a/mailverify?uri=bloomengine&amp;loc=en_US\" target='_blank' onClick=\"ga_subscribe_email();\">Stay updated</a>.</span>";
         }
 
