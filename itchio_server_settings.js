12f68a3a7dac3ba0ff029d91073c62eb5caee774
bloomengine <37038355+bloomengine7@users.noreply.github.com>
2018-08-27 00:37:18 +0800

    z

---
+++ itchio_server_settings.js
@@ -0,0 +1,4 @@
+
+config.debugMode=0;
+config.game_url="https://bloomengine.com/arcadiaheights/"; //used by save game links if game within in iframe
+config.ga_id="arcadia-itchio"; //for google analytics to identify where game loaded. Note that it can also be passed in as querystring index?source=itchio from the iframe parent. Don't do both. Choose one.
